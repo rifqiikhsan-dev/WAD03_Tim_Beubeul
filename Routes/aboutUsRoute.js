a14d1d3731c981d4ab41f445ba0c607299bc623b
Rigan Nur Fauzi <riganfauzi@gmail.com>
2025-09-17 21:59:35 +0700

    feat(aboutus): create about us routes

--- Routes/aboutUsRoute.js
+++ Routes/aboutUsRoute.js
@@ -1,1 +1,39 @@
 //IMPL ABOUT US ROUTE
+const express = require("express");
+const router = express.Router();
+
+const anggota = [
+    {
+        name: "Rigan Nur Fauzi",
+        NIM: "24120300008"
+    },
+    {
+        name: "Adit Praditia",
+        NIM: "24120300004"
+    },
+    {
+        name: "Rifqi Ikhsan Rizkillah",
+        NIM: "24120300007"
+    }
+]
+
+router.get('/:user', (req, res) => {
+    const requestedUser = req.params.user;
+
+    const foundUser = anggota.find(user => {
+    return user.name.toLowerCase().replace(/\s/g, '') === requestedUser.toLowerCase().replace(/\s/g, '') ||
+           user.NIM === requestedUser;
+  });
+
+  if (foundUser) {
+    // Jika pengguna ditemukan
+    res.json(foundUser);
+  } else {
+    // Jika pengguna tidak ditemukan
+    res.status(404).json({
+      message: "User not found"
+    });
+  }
+});
+
+module.exports = router;
