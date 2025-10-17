8c7cae8ba51888c4f616c5f98c2a96807f4a0089
rifqiikhsan45 <rifqiikhsan45@gmail.com>
2025-10-17 22:27:47 +0700

    switch data source from JSON file to database

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,32 +1,36 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../Config/database");
 
-const UserModel = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const UserModel = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  tableName: "users",
-  timestamps: true, 
-});
+  {
+    tableName: "users",
+    timestamps: false
+  }
+);
 
 module.exports = UserModel;
